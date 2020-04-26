94988f5b005b8e4933b23d1d2001c98a8335518d
Manish Shankla <manish@Manishs-MacBook-Pro-2.local>
2020-04-26 09:41:52 -0700

    working initial front-end commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LineChart from './LineChart.js';
+import {getData} from './dataFetcher.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// App 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: getData()
+    };
+  }
+
+  componentDidMount() {
+    window.setInterval(() => {
+      this.setState({
+        data: getData()
+      })
+    }, 5000)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="main chart-wrapper">
+          <LineChart
+            data={this.state.data[0].data}
+            title={this.state.data[0].title}
+            color="#3E517A"
+          />
+        </div>
+      </div>
+    );
+  }
 }
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
---
+++ src/dataFetcher.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+export function getData() {
+    let data = [];
+  
+    data.push({
+      title: 'Visits',
+      data: getRandomDateArray(150)
+    });
+  
+    return data;
+  }
+  
+  export function getRandomDateArray(numItems) {
+    // Create random array of objects (with date)
+    let data = [];
+    let baseTime = new Date('2018-05-01T00:00:00').getTime();
+    let dayMs = 24 * 60 * 60 * 1000;
+    for(var i = 0; i < numItems; i++) {
+      data.push({
+        time: new Date(baseTime + i * dayMs),
+        value: Math.round(20 + 80 * Math.random())
+      });
+    }
+    return data;
+  }
